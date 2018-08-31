61ed4a596fe53393da91bab4c9943378614aee38
Violet Yeh <violetsyeh@gmail.com>
2018-08-31 10:43:38 -0700

    Minor edits to model/homepage.html

--- model.py
+++ model.py
@@ -33,7 +33,7 @@ def connect_to_db(app):
     """Connect the database to our Flask app."""
 
     # Configure to use our PstgreSQL database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///scores'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///hangman'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
